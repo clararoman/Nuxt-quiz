a08fbde4ba90547fe8539951eddaf9a6b055e5a5
Clara <clararoman96@gmail.com>
2020-03-31 12:36:58 +0200

    added logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>
+  <b-container class="outer">
+    <div id="header">
+      <div class="logo"></div>
+    </div>
     <nuxt />
-  </div>
+  </b-container>
 </template>
 
 <script>
 </script>
 
 <style lang="scss">
-.container {
-  width: 80%;
+html {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+.outer {
+  margin-top: 2em;
+}
+.logo {
+  background-color: #e8e8e8;
+  width: 2em;
+  height: 2em;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <b-container>
-    <b-col class="column"></b-col>
+  <b-container class="home">
+    <b-row class="presentation">
+      <b-col>Hello, I am Cloba Boba</b-col>
+    </b-row>
+    <b-row>
+      <b-col class="column"></b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -9,8 +14,16 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.container {
+  .home {
+    margin-top: 2em;
+  }
+}
 .column {
   background-color: #e8e8e8;
-  height: 2em;
+  height: 5em;
+  &.column {
+    margin-right: 1em;
+  }
 }
 </style>
